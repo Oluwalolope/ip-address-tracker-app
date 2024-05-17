f240dcc72b25e9891a86cd3e67d434522db761bc
Oluwalolope <134228462+Oluwalolope@users.noreply.github.com>
2024-05-17 02:16:22 +0100

    Succesfully rendered the map

--- app/js/app.js
+++ app/js/app.js
@@ -13,12 +13,31 @@ const apiKey = 'at_d6YdRmS1XEgSSJqHnZJHcIXevbLJS';
 //Regex
 const domainRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
 
+//map and tiles
+const map = L.map('map').setView([51.505, -0.09], 14);
+const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution =
+  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+
+const tiles = L.tileLayer(tileUrl, { maxZoom: 19, attribution });
+tiles.addTo(map);
+
+//adding a custom marker
+const myIcon = L.icon({
+    iconUrl: 'assets/images/icon-location.svg',
+    iconSize: [38, 45],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+
+const marker = L.marker([51.5, -0.09], {icon: myIcon}).addTo(map);
+
+
 //Listen for the form being submitted
 inputForm.addEventListener('submit', (e) => {
   e.preventDefault(); //prevent the page from refreshing
   let input = inputForm.querySelector('input[type=text]').value;
 
-  console.log(input);
   getIpAddressInfo(input);
 });
 
@@ -44,9 +63,7 @@ const getIpAddressInfo = async (input) => {
   }
   const data = await response.json();
 
-  console.log(data);
-
-  domainRegex.test(input)
+  domainRegex.test(input) && data.domains
     ? outputAddress.innerHTML = `<h3>Domain</h3>
             <p>${data.domains[0]}</p>`
     : outputAddress.innerHTML = `<h3>ip address</h3>
@@ -58,5 +75,9 @@ const getIpAddressInfo = async (input) => {
 
   outputLocation.textContent = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
   outputTimezone.textContent = `GMT ${data.location.timezone}`;
+
+  //render map
+  map.setView([data.location.lat, data.location.lng]);
+  marker.setLatLng([data.location.lat, data.location.lng]);
 };
 getIpAddressInfo(null);
